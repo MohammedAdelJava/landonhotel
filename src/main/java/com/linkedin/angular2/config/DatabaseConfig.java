5cd40d20b3a8ac9e92ed24545ced1c2e013794c3
mohammedadeljava <mohammedadeljavadev@gmail.com>
2018-04-09 16:52:39 +0200

    First Commit

--- src/main/java/com/linkedin/angular2/config/DatabaseConfig.java
+++ src/main/java/com/linkedin/angular2/config/DatabaseConfig.java
@@ -1,11 +1,25 @@
 package com.linkedin.angular2.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import javax.activation.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaRepositories("com.linkedin.angular2.repository")
 @EnableTransactionManagement
+@Configuration
 public class DatabaseConfig {
 
+	 	@Bean
+	    @Primary
+	    @ConfigurationProperties(prefix = "spring.datasource")
+	    public DataSource dataSource() {
+	        return (DataSource) DataSourceBuilder.create().build();
+	    }
+	
 }
